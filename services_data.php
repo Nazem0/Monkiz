4ec2f907ea9f504178c6da393d79b7722aa81383
Nazem0 <109486038+Nazem0@users.noreply.github.com>
2023-06-12 15:13:14 +0300

    Delete services_data.php

--- services_data.php
+++
@@ -1,7 +0,0 @@
-<?php
-$popular_services=[
-    ["img"=>"https://app.openbay.com/img/ic-oil-change.a1ccac6a.svg","name"=>"Oil Change","price"=>120,"requests"=>3120,"providers"=>"http://localhost/monkiz/"],
-    ["img"=>"https://app.openbay.com/img/ic-brake-pads.b6400bc0.svg","name"=>"Brake Pads","price"=>320,"requests"=>2110,"providers"=>"http://localhost/monkiz/"],
-    ["img"=>"https://app.openbay.com/img/ic-general-diagnosis.c429eb28.svg","name"=>"General Diagnosis","price"=>400,"requests"=>240,"providers"=>"http://localhost/monkiz/"]
-];
-?>
